fbf568363105a654183637ad59c603b6ec9c5218
mrsherobrine <mrsherobrinenaomi@gmail.com>
2021-08-22 16:49:59 +0200

    working prototype

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const FileSaver = require('file-saver');
 
+var player1;
+var player2;
+var epic;
+var moveCounter;
 function smallBrain() {
-    document.getElementById("notitle").innerHTML = "NOT ELLU";
+    player1 = document.getElementById("input1").value;
+    document.getElementById("notitle").innerHTML = player1;
+}
+
+function beeegBrain() {
+    player2 = document.getElementById("input2").value;
+    document.getElementById("yestitle").innerHTML = player2
+    epic = player1+" vs "+player2;
+    document.getElementById("output").innerHTML = epic;
+}
+
+function notation() {
+    var moveinput = document.getElementById("inputnot").value;
+    if (player1 != null && player2 != null) {
+        epic = epic + "\n" + moveinput;
+        document.getElementById("inputnot").innerHTML = "";
+        document.getElementById("output").innerHTML = epic;
+    } else {
+        document.getElementById("inputnot").disabled;
+    }
 }
